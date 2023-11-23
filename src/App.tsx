0cde167e2cd7bed3d9a74d21f92b8d740c0b4817
alex <cebanoleksandr@gmail.com>
2023-11-23 20:05:33 +0200

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
 import './App.scss';
 
 export const App = () => {
   return (
-    <div className="app"></div>
+    <div className="app">
+      <header className="header">
+        <Link to="/" className="nav-link">
+          Formula
+        </Link>
+      </header>
+
+      <div className="container">
+        <Outlet />
+      </div>
+    </div>
   );
 }
